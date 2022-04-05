44ca9cd28c3fbdc57760f5e60f9904bf07f28879
Shivanilonare2 <shivanilonare.ashok98@gmail.com>
2022-04-05 23:12:56 +0530

    initial change

--- src/com/company/CWH04_literals.java
+++ src/com/company/CWH04_literals.java
@@ -3,7 +3,7 @@ package com.company;
 public class CWH04_literals {
     public static void main(String[] args) {
         byte age = 34;
-        System.out.println(age);
+        System.out.println("age"+age);
 
     }
 }
